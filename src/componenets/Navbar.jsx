463f9cdb1349a775ecffbde4a63036ea13ac2265
vicky262600 <vickyjoshi2604@gmail.com>
2024-06-12 15:47:16 -0400

    responsive nevbar done

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,27 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link as LinkR } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
+import {Bio} from '../data/constants'
+import MenuIcon from '@mui/icons-material/Menu';
 
-// const Nav = styled.div`
-// background-color: ${({ theme }) => theme.bg};
-// height: 80px;
-// display: flex;
-// align-items: center;
-// justify-content: center;
-// font-size: 1rem;
-// position: sticky;
-// top: 0;
-// z-index: 10;
-// color: white;
-// `
-const NavbarContainer = styled.div`
-// width: 100%;
-// max-width: 1200px;
-// padding: 0 24px;
-// display: flex;
-// align-items: center;
-// justify-content: space-between;
-// font-size: 1rem;
+const Nav = styled.div`
 background-color: ${({ theme }) => theme.bg};
 height: 80px;
 display: flex;
@@ -33,9 +16,20 @@ top: 0;
 z-index: 10;
 color: white;
 `
+const NavbarContainer = styled.div`
+width: 100%;
+max-width: 1200px;
+padding: 0 24px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+font-size: 1rem;
+`
 
 const NavLogo = styled(LinkR)`
 width: 80%;
+font-weight: 500;
+font-size: 18px;
 padding: 0 6px;
 font-weight: 500;
 font-size: 18px;
@@ -51,6 +45,33 @@ const NavItem = styled.ul`
   gap: 32px;
   padding: 0 6px;
   list-style: none;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+const MobileMenu = styled.ul`
+ width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 16px;
+  padding: 0 6px;
+  list-style: none;
+  width: 100%;
+  padding: 12px 40px 24px 40px;
+  background: ${({ theme }) => theme.card_light + 99};
+  position: absolute;
+  top: 80px;
+  right: 0;
+
+  transition: all 0.6s ease-in-out;
+  transform: ${({ isOpen }) =>
+    isOpen ? "translateY(0)" : "translateY(-100%)"};
+  border-radius: 0 0 20px 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  z-index: ${({ isOpen }) => (isOpen ? "1000" : "-1000")};
 `
 const NavLink = styled.a`
 color: ${({ theme }) => theme.text_primary};
@@ -61,9 +82,6 @@ text-decoration: none;
 &:hover {
   color: ${({ theme }) => theme.primary};
 }
-@media screen and (max-width: 768px){
-  display; none;
-}
 `
 const ButtonContainer = styled.div`
 width: 80%;
@@ -72,8 +90,8 @@ display: flex;
 justify-content: end;
 align-items: center;
 padding: 0 6px;
-@media screen and (max-width: 768px){
-  display; none;
+@media screen and (max-width: 768px) {
+  display: none;
 }
 `
 const GithubButton = styled.a`
@@ -93,15 +111,30 @@ const GithubButton = styled.a`
     background: ${({ theme }) => theme.primary};
     color: ${({ theme }) => theme.text_primary};
   }
+  
+`
+
+const MobileIcon = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  display: none;
+  @media screen and (max-width: 768px){
+    display: block;
+  }
 `
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const theme = useTheme();
   return (
-    // <Nav>
+    <Nav>
       <NavbarContainer>
         <NavLogo to="/">
           Portfolio
         </NavLogo>
+        <MobileIcon onClick={() => setIsOpen(!isOpen)} ><MenuIcon style={{color:"inherit"}}/></MobileIcon>
 
         <NavItem>
           <NavLink href="#About">About</NavLink>
@@ -111,12 +144,24 @@ const Navbar = () => {
           <NavLink href="#Education">Education</NavLink>
         </NavItem>
 
+        {isOpen && <MobileMenu isOpen={isOpen}>
+          <NavLink onClick={()=> setIsOpen(!isOpen)} href="#About">About</NavLink>
+          <NavLink onClick={()=> setIsOpen(!isOpen)} href="#Skills">Skills</NavLink>
+          <NavLink onClick={()=> setIsOpen(!isOpen)} href="#Experience">Experience</NavLink>
+          <NavLink onClick={()=> setIsOpen(!isOpen)} href="#projects">Projects</NavLink>
+          <NavLink onClick={()=> setIsOpen(!isOpen)} href="#Education">Education</NavLink>
+          <GithubButton href={Bio.github} target='_blank' style={{
+            background: theme.primary,
+            color: theme.text_primary,
+          }}>Github Profile</GithubButton>
+        </MobileMenu>}
+
         <ButtonContainer>
-          <GithubButton>Github Profile</GithubButton>
+          <GithubButton href={Bio.github} target='_blank'>Github Profile</GithubButton>
         </ButtonContainer>
 
-    </NavbarContainer>
-    // {/* </Nav> */}
+       </NavbarContainer>
+    </Nav> 
   )
 }
 
